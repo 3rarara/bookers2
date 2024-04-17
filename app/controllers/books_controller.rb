a5dec2522925f4bcfa14ba4a435f1470c018cf32
3rarara <161809435+3rarara@users.noreply.github.com>
2024-04-17 03:21:26 +0000

    [Finish]Flash

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,8 +6,12 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(@book.id)
+    if @book.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to book_path(@book.id)
+    else
+      render :new
+    end
   end
 
   def index
@@ -24,8 +28,12 @@ class BooksController < ApplicationController
 
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
-    redirect_to book_path
+    if @book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
+      redirect_to book_path
+    else
+      render :update
+    end
   end
 
   def destroy
